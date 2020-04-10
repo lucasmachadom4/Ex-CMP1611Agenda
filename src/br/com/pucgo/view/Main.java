d1acdf7885bf9b199c1ae86caebbebf83f08d8ee
Lucas M Marques <50883432+lucasmachadom4@users.noreply.github.com>
2020-04-10 14:58:36 -0300

    Insertes no banco

--- src/br/com/pucgo/view/Main.java
+++ src/br/com/pucgo/view/Main.java
@@ -10,23 +10,32 @@ import br.com.pucgo.entidade.Tipo;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		AgendaController agendaController = new AgendaController();
+		////
+		Agenda agenda = new Agenda();		
+		agenda.setNome("Altair Flior");		
+		agenda.setCategoria(new Categoria(4));		
 		
-		Agenda agenda = new Agenda();
+		ArrayList<Telefone> listaTelefones1 = new ArrayList<Telefone>();		
+		listaTelefones1.add(new Telefone("(62) 0000-0009", new Tipo(3) ) );
+		listaTelefones1.add(new Telefone("(62) 9 5566-8009", new Tipo(2) ) );
+		listaTelefones1.add(new Telefone("(62) 9 5566-8009", new Tipo(1) ) );
+		agenda.setTelefones(listaTelefones1);
 		
-		agenda.setNome("Melisandra Alq");		
-		agenda.setCategoria(new Categoria(4));
+		agendaController.adicionarNaAgenda(agenda);		
+		/////
+		Agenda agenda2 = new Agenda();
+		agenda2.setNome("Melisandra Alq");		
+		agenda2.setCategoria(new Categoria(4));		
 		
+		ArrayList<Telefone> listaTelefones2 = new ArrayList<Telefone>();		
+		listaTelefones2.add(new Telefone("(62) 0000-0009", new Tipo(3) ) );
+		listaTelefones2.add(new Telefone("(62) 9 5566-8009", new Tipo(2) ) );
+		agenda2.setTelefones(listaTelefones2);
 		
-		ArrayList<Telefone> listaTelefones = new ArrayList<Telefone>();		
-		listaTelefones.add(new Telefone("(62) 0000-0009", new Tipo(3) ) );
-		listaTelefones.add(new Telefone("(62) 9 5566-8009", new Tipo(2) ) );
-		agenda.setTelefones(listaTelefones);
-		
-		agendaController.adicionarNaAgenda(agenda);
-		
+		agendaController.adicionarNaAgenda(agenda2);
+		/////		
 		agendaController.exibirAgenda();
 	}
 
